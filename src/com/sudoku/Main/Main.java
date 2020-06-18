5146924fffd50a2bedfc7a375f79d0f3274d455e
Suraj Nair <sagin2711@gmail.com>
2020-06-18 22:11:54 +0530

    Input file addded : one can input sudoku in input.csv file

--- src/com/sudoku/Main/Main.java
+++ src/com/sudoku/Main/Main.java
@@ -3,22 +3,32 @@ package com.sudoku.Main;
 import com.sudoku.Utility.Board;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Board sudoku = new Board(9);
-		int [][] board = {
-				{5,3,0,0,7,0,0,0,0},
-				{6,0,0,1,9,5,0,0,0},
-				{0,9,8,0,0,0,0,6,0},
-				{8,0,0,0,6,0,0,0,3},
-				{4,0,0,8,0,3,0,0,1},
-				{7,0,0,0,2,0,0,0,6},
-				{0,6,0,0,0,0,2,8,0},
-				{0,0,0,4,1,9,0,0,5},
-				{0,0,0,0,8,0,0,7,9},
 
-		};
+		Board sudoku = new Board(9);
+		int [][] board = new int[9][9] ;
+
+		try {
+			Scanner scanner = new Scanner(new File("input.csv"));
+			for( int i = 0 ; scanner.hasNextLine() ; i++ ){
+				Scanner parser = new Scanner(scanner.nextLine());
+				parser.useDelimiter(",");
+				for( int j = 0 ; parser.hasNext() ; j++ ){
+					board[i][j] = parser.nextInt();
+				}
+				parser.close();
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
 		sudoku.setBoard(board);
 		sudoku.show();
 		sudoku.solve();
